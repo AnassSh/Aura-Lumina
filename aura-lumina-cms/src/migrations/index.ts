f8d2a3791bc786ef3318838013dfffdaffce2194
AnassSh <anass.shayab@hightech.edu>
2026-02-24 23:33:27 +0000

    fix(cms): remove broken migration 20260224_231109, keep only incremental migrations

--- aura-lumina-cms/src/migrations/index.ts
+++ aura-lumina-cms/src/migrations/index.ts
@@ -1,6 +1,5 @@
 import * as migration_20260222_add_shops_products from './20260222_add_shops_products';
 import * as migration_20260223_add_shops_image_url from './20260223_add_shops_image_url';
-import * as migration_20260224_231109 from './20260224_231109';
 import * as migration_20260224_lookbooks_beauty_orders from './20260224_lookbooks_beauty_orders';
 
 export const migrations = [
@@ -15,13 +14,8 @@ export const migrations = [
     name: '20260223_add_shops_image_url',
   },
   {
-    up: migration_20260224_231109.up,
-    down: migration_20260224_231109.down,
-    name: '20260224_231109',
-  },
-  {
     up: migration_20260224_lookbooks_beauty_orders.up,
     down: migration_20260224_lookbooks_beauty_orders.down,
-    name: '20260224_lookbooks_beauty_orders'
+    name: '20260224_lookbooks_beauty_orders',
   },
 ];
